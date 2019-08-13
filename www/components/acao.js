33a271e2d4499e79eb9902ec778d22bade6d1999
LucasNeves17 <lucasneves_01@hotmail.com>
2019-08-13 18:33:18 +0900

    JS não está funcionando

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,32 @@
-// This is a JavaScript file
+$(document).on("click","#calcular",function(){
 
+  var feminino;
+  var masculino;
+  var peso;
+  var altura;
+  var idade;
+  var nivel;
+  var resultado;
+
+   idade = $("#idade").val();
+   peso = $("#peso").val();
+   altura = $("#altura").val();
+   feminino = $("input[id = 'mulher']:checked").val();
+   masculino = $("input[id = 'homem']:checked").val();
+   nivel = $("#nivelAtividade").val();
+
+   altura = altura * 100;
+
+  if(sexomasculino == 'masculino'){
+
+    resultado = nivel *(66 + ((13,7 * peso) + (5 * altura) - (6.8 * idade)));
+    $("#visor").val(resultado);
+
+  }
+  else if(sexofeminino == 'feminino'){
+
+    resultado = nivel *(655 + ((13,7 * peso) + (1.8 * altura) - (4.7 * idade)));
+       $("#visor").val(resultado);
+
+  }
+}
