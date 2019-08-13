4aaee161594d3af1c0ed8fdbae8fcbe5fa2f1dee
LucasNeves17 <lucasneves_01@hotmail.com>
2019-08-14 03:35:40 +0900

    javaScript quase funcionando

--- www/components/acao.js
+++ www/components/acao.js
@@ -7,26 +7,29 @@ $(document).on("click","#calcular",function(){
   var idade;
   var nivel;
   var resultado;
+ 
+ 
 
-   idade = $("#idade").val();
-   peso = $("#peso").val();
-   altura = $("#altura").val();
-   feminino = $("input[id = 'mulher']:checked").val();
-   masculino = $("input[id = 'homem']:checked").val();
-   nivel = $("#nivelAtividade").val();
+   $idade = $("#idade").val();
+   $peso = $("#peso").val();
+   $altura = $("#altura").val();
+   $feminino = $("input[id = 'mulher']:checked").val();
+   $masculino = $("input[id = 'homem']:checked").val();
+   $nivel = $("#nivelAtividade").val();
 
-   altura = altura * 100;
+  if($masculino == "#homem"){
 
-  if(sexomasculino == 'masculino'){
-
-    resultado = nivel *(66 + ((13,7 * peso) + (5 * altura) - (6.8 * idade)));
-    $("#visor").val(resultado);
-
-  }
-  else if(sexofeminino == 'feminino'){
-
-    resultado = nivel *(655 + ((13,7 * peso) + (1.8 * altura) - (4.7 * idade)));
-       $("#visor").val(resultado);
+    $resultado = $nivel * (66 + ((13.7 *  $peso) + (5 * $altura) - (6.8 * $idade)));
+    $("#visor").val($resultado);
 
   }
-}
+  else{
+    $resultado = $nivel *(655 + ((9.6 * $peso) + (1.8 * $altura) - (4.7 * $idade)));
+       $("#visor").val($resultado);
+  }
+});
+$(document).on("click","#limpar",function(){
+  $("#visor").val("");
+    $($resultado).val($resultado);
+    $($)
+});
